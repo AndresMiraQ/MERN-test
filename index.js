8d5484ffbf38db00ac5136d80007384b9698ba95
AndresMiraQ <krhistian1902@gmail.com>
2022-06-16 21:23:50 -0500

    Trabajando con Express

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const http = require('http'); // commonjs
 
 const notes = [
     {
-        title: 'My next trip',
-        body: 'I would like to go to Spain'
+        id: 1,
+        'content': 'HTML is easy',
+        'date': '2019-05-30T17:30:31.098Z',
+        'important': true
     },
     {
-        title: 'Habits to work on',
-        body: 'Excercise, Sleep, Eat, etc'
+        id: 2,
+        'content': 'Browser can execute only Javascript',
+        'date': '2019-05-30T18:39:34.091Z',
+        'important': false
+    },
+    {
+        id: 3,
+        'content': 'GET and POST are the most important methods of HTTP protocol',
+        'date': '2019-05-30T19:20:35.091Z',
+        'important': true
     }
 ];
 
