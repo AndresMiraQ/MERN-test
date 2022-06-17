32d966a4c5acf87cd6db7fdbba9e30cddd56fe1d
AndresMiraQ <krhistian1902@gmail.com>
2022-06-16 21:49:11 -0500

    GET's and DELETE has created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const http = require('http'); // commonjs
-//import http from 'http'; // esmodules 
+const express = require('express');
+const app = express();
 
 const notes = [
     {
@@ -22,11 +22,29 @@ const notes = [
     }
 ];
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(notes));
-    });
-    
-    const PORT = 3001;
-    app.listen(PORT);
-    console.log(`Server running at http://localhost:${PORT}/`);
+app.get('/', (req, res) => { 
+    res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+                    // : dinamic route  
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const note = notes.find(note => note.id === id);
+    note ? res.json(note) : res.status(404).end();
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    notes = notes.filter(note => note.id !== id);
+    res.status(204).end();
+});
+
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
