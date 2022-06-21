5f9527b40034cdb0d0120a31aa91b12981a4e719
AndresMiraQ <krhistian1902@gmail.com>
2022-06-20 23:46:38 -0500

    Confing del linter

--- index.js
+++ index.js
@@ -2,71 +2,69 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-
 let notes = [
-    {
-        id: 1,
-        'content': 'HTML is easy',
-        'date': '2019-05-30T17:30:31.098Z',
-        'important': true
-    },
-    {
-        id: 2,
-        'content': 'Browser can execute only Javascript',
-        'date': '2019-05-30T18:39:34.091Z',
-        'important': false
-    },
-    {
-        id: 3,
-        'content': 'GET and POST are the most important methods of HTTP protocol',
-        'date': '2019-05-30T19:20:35.091Z',
-        'important': true
-    }
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:35.091Z',
+    important: true
+  }
 ];
 
-app.get('/', (req, res) => { 
-    res.send('<h1>Hello World!</h1>');
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+  res.json(notes);
 });
-                    // : dinamic route  
+// : dinamic route
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(identifier => identifier.id === id);
-    note ? res.json(note) : res.status(404).end();
+  const id = Number(req.params.id);
+  const note = notes.find(identifier => identifier.id === id);
+  note ? res.json(note) : res.status(404).end();
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    notes = notes.filter(note => note.id !== id);
-    res.status(204).end();
+  const id = Number(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+  res.status(204).end();
 });
 
 app.post('/api/notes', (req, res) => {
-    const note = req.body;
-    
-    const ids = notes.map(identifier => identifier.id);
-    const maxId = Math.max(...ids);
+  const note = req.body;
 
-    if (!note || !note.content) {
-        return res.status(400).json({ error: 'content missing' });
-    } 
+  const ids = notes.map(identifier => identifier.id);
+  const maxId = Math.max(...ids);
 
-    const newNote = {
-        id: maxId + 1,
-        content: note.content,
-        date: new Date(),
-        important: typeof note.important !== "undefined" ? note.important : false
-    }
-    //notes.push(newNote);
-    notes = [...notes, newNote];
-    res.statusCode(201).json(newNote);
+  if (!note || !note.content) {
+    return res.status(400).json({ error: 'content missing' });
+  }
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    date: new Date(),
+    important: typeof note.important !== 'undefined' ? note.important : false
+  };
+    // notes.push(newNote);
+  notes = [...notes, newNote];
+  res.statusCode(201).json(newNote);
 });
-
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
