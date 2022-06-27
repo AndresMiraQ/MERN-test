b6bf94f75a7b781453aad82ddbb5bc4fbe55a8a4
CristianMira <krhistian1902@gmail.com>
2022-06-26 20:22:38 -0500

    Ready to deploy

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(logger);
 
 let notes = [
@@ -28,6 +27,12 @@ let notes = [
     date: "2019-05-30T19:20:35.091Z",
     important: true,
   },
+  {
+    id: 4 ,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:35.091Z",
+    important: true,
+  },
 ];
 
 app.get("/", (req, res) => {
@@ -37,10 +42,12 @@ app.get("/", (req, res) => {
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
+
 // : dinamic route
 app.get("/api/notes/:id", (req, res) => {
   const id = Number(req.params.id);
   const note = notes.find((identifier) => identifier.id === id);
+
   note ? res.json(note) : res.status(404).end();
 });
 
